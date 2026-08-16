7d3d81778a1b42c43216654cfd37a1331cdbe53b
YossefHassan19 <yossef.hassan678@gmail.com>
2026-08-16 15:42:30 +0300

    Part01 Question01

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@
     {
         static void Main(string[] args)
         {
+            #region Part01 Question01
+
+            //a)What is Abstraction in Object-Oriented Programming?
+            //Hiding implementation details and showing only the essential feature
+
+            //b)Why is abstraction considered one of the four pillars of OOP?
+            //1-Reduce complexity
+            //2-Improve maintainability
+            //3-Enhance security
+            //4-Promotes flexibility and reuseability
+
+            #endregion
 
         }
     }
